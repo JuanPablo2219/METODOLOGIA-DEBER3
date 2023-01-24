e319312ea52f6ea0a305dec7664cf46725a7ccaf
JuanPablo2219 <116099460+JuanPablo2219@users.noreply.github.com>
2023-01-24 16:08:13 -0500

    hola

---
+++ EJERCICIO1.js
@@ -0,0 +1,6 @@
+let nums =[7,4,0,9,7,6,3,8]
+let cont =0;
+for (let i = 0; i < nums.length; i++){
+    cont++;
+}
+console.log(cont)
---
+++ EJERCICIO2.js
@@ -0,0 +1,10 @@
+let nums=[0,2,4,8]
+let jota = " j "
+let i = 0
+
+for (let i = 0; i < nums.length; i++) {
+  for (let j = 0; j < jota.length; j++) {
+    console.log(nums[i] + " " + jota[j]);
+  }
+}
+
